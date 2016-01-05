897cc61b5232b4654249b2b83460200afdc2522e
464813006@qq.com <464813006@qq.com>
2016-01-06 00:07:27 +0800

    数据库相关
    第一步完成
    手腕累

---
+++ src/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -0,0 +1,39 @@
+package com.coolweather.app.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CoolWeatherOpenHelper extends SQLiteOpenHelper{
+	public static final String CREATE_PROVICE = "create table Provice ("
+			+ "id integer primary key antoincrement, "
+			+ "provice_name text, "
+			+ "provice_code text)"; 
+	public static final String CREATE_CITY = "create table City ("
+			+ "id integer primary key autoincrement, "
+			+ "city_name text, "
+			+ "city_code text, "
+			+ "province_id integer)";
+
+	public static final String CREATE_COUNTY = "create table County ("
+			+ "id integer primary key autoincrement, "
+			+ "county_name text, "
+			+ "county_code text, "
+			+ "city_id integer)";
+	
+	public CoolWeatherOpenHelper(Context context, String name,
+			CursorFactory factory, int version) {
+		super(context, name, factory, version);
+	}
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(CREATE_PROVICE);
+		db.execSQL(CREATE_CITY);
+		db.execSQL(CREATE_COUNTY);
+	}
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion,int newVersion) {
+		
+	}
+}
